a994dd94daee4610f433e27c47ee694a14f8ab3e
unknown <raulmartinezgimenez21@gmail.com>
2025-01-21 14:06:08 +0100

    proyecto basico para empezar a implementar la utenticacion

---
+++ src/main/java/com/example/spring_security_oauth_keycloack/SpringSecurityOauthKeycloackApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_oauth_keycloack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauthKeycloackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauthKeycloackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_security_oauth_keycloack/SpringSecurityOauthKeycloackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_oauth_keycloack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauthKeycloackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
